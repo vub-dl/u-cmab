13b96243e07427f0ff7feaca781c1daaf55768fc
jeroen berrevoets <jeroen.berrevoets@gmail.com>
2019-05-17 11:03:28 +0200

    Update to dynamic enabled URF

--- u_cmab/fourier.py
+++ u_cmab/fourier.py
@@ -6,9 +6,10 @@ import pylift
 import itertools
 
 class Fourier:
-	def __init__(self, env, order=2):
+	def __init__(self, env, order=2, regular=False):
 		self.env = env
 		self.order = order
+		self.regular = regular
 
 		columns = []
 		for i in range(env.dim_count):
@@ -30,13 +31,20 @@ class Fourier:
 
 		return np.random.binomial(1, p)
 
-	def run(self, epsilon=.1, alpha=.005, tau=.2, window=100, lifetime=30000):
-		w = np.zeros((self.order + 1) ** self.env.dim_count * 2) # see Fourier paper
+	def run(self, epsilon=.1, alpha=.005, tau=.2, window=100, lifetime=30000, decay_eps=.999):
+		start_eps = 1.0
+
+		w_u, w_r = np.zeros((self.order + 1) ** self.env.dim_count * 2), np.zeros((self.order + 1) ** self.env.dim_count * 2) # see Fourier paper
 		
-		bandit_result = np.array([])
+		bandit_result_u, bandit_result_r = np.array([]), np.array([])
 		optimal_result = np.array([])
-		difference = np.array([])
-		incremental_diff = np.array([])
+		difference_u, difference_r = np.array([]), np.array([])
+		incremental_diff_u, incremental_diff_r = np.array([]), np.array([])
+
+		total_reward_u, total_reward_r = np.array([]), np.array([])
+		tot_r_u, tot_r_r = 0, 0
+		exec_action_u, exec_action_r = np.array([]), np.array([])
+		tot_ac_u, tot_ac_r = 0, 0
 
 		drift_moments = np.array([])
 
@@ -48,23 +56,41 @@ class Fourier:
 		for i in range(lifetime):
 			
 			# choose action (e-greedy)
-			a = -1
-			if np.random.binomial(1, epsilon):
-				a = np.random.binomial(1, .5)
+			a_u, a_reg = -1, -1
+			if np.random.binomial(1, max(epsilon, start_eps)):
+				a_reg = np.random.binomial(1, .5)
+				a_u = a_reg
 			else:
 				feedback = np.array([])
+				reward = np.array([])
 				for a in range(2):
-					feedback = np.append(feedback, self.estimate(s, a, w) - (tau * a))
-				a = np.argmax(feedback)
+					reward = np.append(reward, self.estimate(s, a, w_r))
+					feedback = np.append(feedback, self.estimate(s, a, w_u) - (tau * a))
+				a_reg = np.argmax(reward)
+				a_u = np.argmax(feedback)
 			
-					
+			start_eps*=decay_eps		
 
 			# apply action on environment
-			n_s, r = self.env.choose_cause(a)
+			n_s, r_r = self.env.choose_cause(a_reg)
+			r_u = self.what_if_cause(s, a_u)
+
+			tot_r_u += r_u
+			total_reward_u = np.append(total_reward_u, tot_r_u)
+			tot_ac_u += 1 if a_u != 0 else 0
+			exec_action_u = np.append(exec_action_u, tot_ac_u)
+
+			tot_r_r += r_r
+			total_reward_r = np.append(total_reward_r, tot_r_r)
+			tot_ac_r += 1 if a_reg != 0 else 0
+			exec_action_r = np.append(exec_action_r, tot_ac_r)
+
+
 			if self.env.sudden_drift and self.env.current_drift:
 				drift_moments = np.append(drift_moments, self.env.time)
 			
-			w = self.update_w(w, r - self.estimate(s, a, w), s, a, alpha)
+			w_u = self.update_w(w_u, r_u - self.estimate(s, a_u, w_u), s, a_u, alpha)
+			w_r = self.update_w(w_r, r_r - self.estimate(s, a_reg, w_r), s, a_reg, alpha)
 			
 			s_cause = self.get_sim_cause(s, tau)
 
@@ -76,16 +102,19 @@ class Fourier:
 
 			RP_history.append([*s, RP_C, RP_E, E1, E0, s_cause])
 			
-			bandit_result = np.append(bandit_result, a)
+			bandit_result_u = np.append(bandit_result_u, a_u)
+			bandit_result_r = np.append(bandit_result_r, a_reg)
 			optimal_result = np.append(optimal_result, s_cause)
-			difference = np.append(difference, abs(a - s_cause))
-			incremental_diff = np.append(incremental_diff, np.average(difference[-window:]))
+			difference_u = np.append(difference_u, abs(a_u - s_cause))
+			difference_r = np.append(difference_r, abs(a_reg - s_cause))
+			incremental_diff_u = np.append(incremental_diff_u, np.average(difference_u[-window:]))
+			incremental_diff_r = np.append(incremental_diff_r, np.average(difference_r[-window:]))
 			
 			s = n_s
 		
 		self.RP_run_history = self.RP_run_history.append(pd.DataFrame(RP_history, columns=self.RP_run_history_cols))
 
-		return incremental_diff, drift_moments
+		return incremental_diff_u, drift_moments, incremental_diff_r, total_reward_u, exec_action_u, total_reward_r, exec_action_r
 			
 	def gather_c(self):
 		cart_prod = list(itertools.product(np.arange(0, self.order+1), repeat=self.env.dim_count))
--- u_cmab/static_UM.py
+++ u_cmab/static_UM.py
@@ -19,7 +19,6 @@ class Static_UM:
 				uplift modeling, E is the registered outcome and optimal_cause is what the 
 				oracle simulation would apply for C.
 		"""
-
 		self.RP_run_history = RP_run_history
 	
 	def run(self, static_dataset_size=6000, total_experiment_count=14000, tau=.2, window=100, PH_alpha=.005, PH_init_length=200, PH_lambda=.1):
@@ -46,69 +45,100 @@ class Static_UM:
 				of its moving window. 
 		"""
 		
-		# Setup SKL model
-		up = TransformedOutcome((
-			self.RP_run_history.iloc[:,0:self.RP_run_history.shape[1] - 3][0:static_dataset_size], 
-			self.RP_run_history.iloc[:,0:self.RP_run_history.shape[1] - 3][static_dataset_size:total_experiment_count+1]), 
-			col_treatment='C', col_outcome='E', 
-			stratify=None, # No stratification as we need to match states in test set with Fourier
-			sklearn_model=RandomForestRegressor)
+		start_location = 0
 		
-		# Random search over model parameters (3 folds * 50 iterations = 150 fits)
-		# eventual parameters are returned for documentation
-		up.randomized_search(
-			param_distributions={'max_depth': range(2,100), 'min_samples_split': range(2,1000)}, 
-			n_iter=50, n_jobs=10)
-
-		up.fit(**up.rand_search_.best_params_)
-
-
-		# x_test is
-		predictions = up.model.predict(up.x_test)		
-		decisions = [
-			*self.RP_run_history['C'].head(static_dataset_size).tolist(), # the random policy decisions in 'data collection' are also included for evaluation. 
-			*list(map(lambda p: 1 if p>tau else 0, predictions))]		
+		model_decisions = np.zeros(self.RP_run_history.shape[0])
 		optimal_decisions = self.RP_run_history['optimal_cause'].tolist()
 
+		while self.RP_run_history.shape[0] - start_location > static_dataset_size:
+			
+			up, decisions = self.train_new_model(train_size=static_dataset_size, start_location=start_location, tau=tau)
+			if len(decisions) + start_location != self.RP_run_history.shape[0]:
+				raise ValueError(F"Amount of decisions ({len(decisions)}) from start location ({start_location}) does not match with RP_run_history ({self.RP_run_history.shape[0]})")
+			
+			model_decisions[start_location:self.RP_run_history.shape[0] + 1] = decisions
 
-
+			start_location, _ = self.get_PH_location(decisions, start_of_section=start_location, train_size=static_dataset_size)
+			print(start_location)
 
 		difference = np.array([])
 		static_id = np.array([])
 		
-		PH_windowed_avg = 0
-		PH_total_avg = 0		
-		PH_value = np.array([1])
-		PH_total_avgs = np.array([1])
-		PH_windowed_avgs = np.array([1])
-		PH_N = 1
-		for i in range(len(decisions)):
-			difference = np.append(difference, abs(decisions[i] - optimal_decisions[i]))
+		for i in range(len(model_decisions)):
+			difference = np.append(difference, abs(model_decisions[i] - optimal_decisions[i]))
 			static_id = np.append(static_id, np.average(difference[-window:]))
-			
-			if decisions[i] == 1:
-				PH_total_avg = (1 - 1/(PH_N)) * PH_total_avg + 1/(PH_N) * self.RP_run_history.iloc[i,:][F"E{decisions[i]}"]
-				PH_windowed_avg = (1 - PH_alpha) * PH_windowed_avg + PH_alpha * self.RP_run_history.iloc[i,:][F"E{decisions[i]}"]
-				PH_value = np.append(PH_value, abs(PH_total_avg - PH_windowed_avg))
+				
+			"""	
+				PH_windowed_avg = 0
+				PH_total_avg = 0		
+				PH_value = np.array([0])
+				PH_total_avgs = np.array([0])
+				PH_windowed_avgs = np.array([0])
+				PH_N = 1						
+				for i in range(len(decisions[static_dataset_size:])):
+					if decisions[i + static_dataset_size] == 1:
+						PH_total_avg = (1 - 1/(PH_N)) * PH_total_avg + 1/(PH_N) * self.RP_run_history.iloc[i + static_dataset_size + start_location,:]["E1"]
+						PH_windowed_avg = (1 - PH_alpha) * PH_windowed_avg + PH_alpha * self.RP_run_history.iloc[i + static_dataset_size + start_location,:]["E1"]
+						
+						PH_value = np.append(PH_value, abs(PH_total_avg - PH_windowed_avg))
+						PH_windowed_avgs = np.append(PH_windowed_avgs, PH_windowed_avg)
+						PH_total_avgs = np.append(PH_total_avgs, PH_total_avg)
 
-				PH_windowed_avgs = np.append(PH_windowed_avgs, PH_windowed_avg)
-				PH_total_avgs = np.append(PH_total_avgs, PH_total_avg)
+						PH_N += 1
+					else:
+						PH_value = np.append(PH_value, PH_value[-1])
+						PH_windowed_avgs = np.append(PH_windowed_avgs, PH_windowed_avgs[-1])
+						PH_total_avgs = np.append(PH_total_avgs, PH_total_avgs[-1])
 
-				PH_N += 1
-			else:
-				PH_value = np.append(PH_value, PH_value[-1])
-				PH_windowed_avgs = np.append(PH_windowed_avgs, PH_windowed_avgs[-1])
-				PH_total_avgs = np.append(PH_total_avgs, PH_total_avgs[-1])
+					
+				stop_locations = np.where(PH_value[1:]>PH_lambda)[0] + static_dataset_size
+				if stop_locations.size == 0 or stop_locations[stop_locations>static_dataset_size + 1000].size == 0:
+					stop_location = self.RP_run_history.shape[0]
+				else:
+					stop_location = int(np.min(stop_locations[stop_locations>static_dataset_size + 1000]))
 
+				print(F"STOP AT {stop_location + start_location}")
+				end_decisions = np.append(end_decisions, decisions[0:stop_location])
+				s_ids = np.append(s_ids, static_id[0:stop_location])
+				PH_v = np.append(PH_v, PH_value[1:stop_location])
+				start_location += stop_location
+			"""
 
 		# parameters can be consulted using up.rand_search_.best_params_
-		return up, static_id, PH_total_avgs[1:], PH_windowed_avgs[1:], PH_value[1:]
+		return up, static_id #, PH_v #, PH_total_avgs[1:], PH_windowed_avgs[1:], PH_value[1:]
+
+	def get_PH_location(self, decisions, start_of_section=0, train_size=6000, PH_lambda=.2, PH_alpha=.05, PH_offset=2000):
+		action_results = self.RP_run_history[start_of_section + train_size:]["E1"].tolist()
+		decisions_after_random = decisions[train_size:]
+
+		print(f"len(action_results) = {len(action_results)} AND len(decisions_after_r...) = {len(decisions_after_random)}")
+		
+		t_avg, w_avg, n = 0, 0, 1
+		p_vals = np.array([0])
+
+		for i in range(len(decisions_after_random)):
+			if decisions_after_random[i] == 1:
+				t_avg = (1 - 1/n)*t_avg + (1/n)*action_results[i]
+				w_avg = (1 - PH_alpha)*w_avg + PH_alpha*action_results[i]
+
+				p_val = abs(t_avg - w_avg)
+				n+=1			
+			else:
+				p_val = p_vals[-1]
+			
+			p_vals = np.append(p_vals, p_val) # p_vals from start_of_section + train_size
+
+		locations = np.where(p_vals[PH_offset:]>PH_lambda)[0] # evaluate p_vals > PH_lambda from start_of_section + train_size + PH_offset (PH_offset is necessary for stabilised values)
+		if locations.size == 0:
+			return self.RP_run_history.shape[0] + 1, p_vals
+		return np.min(locations) + PH_offset + train_size + start_of_section, p_vals # return location relative to complete RP_run_history
+
 
 	def train_new_model(self, train_size=6000, start_location=0, tau=.2):
 		# Setup SKL model
 		up = TransformedOutcome((
-			self.RP_run_history.iloc[:,0:self.RP_run_history.shape[1] - 3][start_location:train_size+start_location], 
-			self.RP_run_history.iloc[:,0:self.RP_run_history.shape[1] - 3][train_size + start_location:self.RP_run_history.shape[0]+1]), 
+			self.RP_run_history.iloc[start_location:train_size + start_location,0:self.RP_run_history.shape[1] - 3], 
+			self.RP_run_history.iloc[train_size + start_location:self.RP_run_history.shape[0]+1,0:self.RP_run_history.shape[1] - 3]), 
 			col_treatment='C', col_outcome='E', 
 			stratify=None, # No stratification as we need to match states in test set with Fourier
 			sklearn_model=RandomForestRegressor)
@@ -127,4 +157,4 @@ class Static_UM:
 		decisions = [
 			*self.RP_run_history['C'][start_location:train_size+start_location].tolist(), # the random policy decisions in 'data collection' are also included for evaluation. 
 			*list(map(lambda p: 1 if p>tau else 0, predictions))]		
-		return decisions
+		return up, decisions
